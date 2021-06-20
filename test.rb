00e02aadd5b180bfc292e87ba7a165313275aaab
Mark Campbell <nitrodist@gmail.com>
2021-06-19 23:50:51 -0500

    Today I worked with Sophie on a simple ruby gets command to accept user input
    This was pretty fun, definitely doing it again soon.

--- test.rb
+++ test.rb
@@ -1,11 +1,11 @@
-require 'rainbow/refinement'
-using Rainbow
-
-i = 3
-
-puts i + 2
-
-puts "Do #{i + 10}"
+#require 'rainbow/refinement'
+#using Rainbow
+#
+#i = 3
+#
+#puts i + 2
+#
+#puts "Do #{i + 10}"
 
 #puts "sophie 🦄 "
 #puts 12.5
@@ -16,8 +16,19 @@ puts "Do #{i + 10}"
 #puts "some " + "red".color(:red) + " and " + "blue on yellow".color(:blue).background(:yellow)
 #
 #puts "foobar".color(:aqua)
-colors = [:aliceblue, :antiquewhite, :aqua, :aquamarine, :azure, :beige, :bisque, :blanchedalmond, :blueviolet, :brown, :burlywood, :cadetblue, :chartreuse, :chocolate, :coral, :cornflower, :cornsilk, :crimson, :darkblue, :darkcyan, :darkgoldenrod, :darkgray, :darkgreen, :darkkhaki, :darkmagenta, :darkolivegreen, :darkorange, :darkorchid, :darkred, :darksalmon, :darkseagreen, :darkslateblue, :darkslategray, :darkturquoise, :darkviolet, :deeppink, :deepskyblue, :dimgray, :dodgerblue, :firebrick, :floralwhite, :forestgreen, :fuchsia, :gainsboro, :ghostwhite, :gold, :goldenrod, :gray, :greenyellow, :honeydew, :hotpink, :indianred, :indigo, :ivory, :khaki, :lavender, :lavenderblush, :lawngreen, :lemonchiffon, :lightblue, :lightcoral, :lightcyan, :lightgoldenrod, :lightgray, :lightgreen, :lightpink, :lightsalmon, :lightseagreen, :lightskyblue, :lightslategray, :lightsteelblue, :lightyellow, :lime, :limegreen, :linen, :maroon, :mediumaquamarine, :mediumblue, :mediumorchid, :mediumpurple, :mediumseagreen, :mediumslateblue, :mediumspringgreen, :mediumturquoise, :mediumvioletred, :midnightblue, :mintcream, :mistyrose, :moccasin, :navajowhite, :navyblue, :oldlace, :olive, :olivedrab, :orange, :orangered, :orchid, :palegoldenrod, :palegreen, :paleturquoise, :palevioletred, :papayawhip, :peachpuff, :peru, :pink, :plum, :powderblue, :purple, :rebeccapurple, :rosybrown, :royalblue, :saddlebrown, :salmon, :sandybrown, :seagreen, :seashell, :sienna, :silver, :skyblue, :slateblue, :slategray, :snow, :springgreen, :steelblue, :tan, :teal, :thistle, :tomato, :turquoise, :violet, :webgray, :webgreen, :webmaroon, :webpurple, :wheat, :whitesmoke, :yellowgreen]
 
-100.times.with_index do |i|
-  puts "#{i+1} HELLO!".chars.map { |char| Rainbow(char).color(colors.sample) }.join
-end
+# colors = [:aliceblue, :antiquewhite, :aqua, :aquamarine, :azure, :beige, :bisque, :blanchedalmond, :blueviolet, :brown, :burlywood, :cadetblue, :chartreuse, :chocolate, :coral, :cornflower, :cornsilk, :crimson, :darkblue, :darkcyan, :darkgoldenrod, :darkgray, :darkgreen, :darkkhaki, :darkmagenta, :darkolivegreen, :darkorange, :darkorchid, :darkred, :darksalmon, :darkseagreen, :darkslateblue, :darkslategray, :darkturquoise, :darkviolet, :deeppink, :deepskyblue, :dimgray, :dodgerblue, :firebrick, :floralwhite, :forestgreen, :fuchsia, :gainsboro, :ghostwhite, :gold, :goldenrod, :gray, :greenyellow, :honeydew, :hotpink, :indianred, :indigo, :ivory, :khaki, :lavender, :lavenderblush, :lawngreen, :lemonchiffon, :lightblue, :lightcoral, :lightcyan, :lightgoldenrod, :lightgray, :lightgreen, :lightpink, :lightsalmon, :lightseagreen, :lightskyblue, :lightslategray, :lightsteelblue, :lightyellow, :lime, :limegreen, :linen, :maroon, :mediumaquamarine, :mediumblue, :mediumorchid, :mediumpurple, :mediumseagreen, :mediumslateblue, :mediumspringgreen, :mediumturquoise, :mediumvioletred, :midnightblue, :mintcream, :mistyrose, :moccasin, :navajowhite, :navyblue, :oldlace, :olive, :olivedrab, :orange, :orangered, :orchid, :palegoldenrod, :palegreen, :paleturquoise, :palevioletred, :papayawhip, :peachpuff, :peru, :pink, :plum, :powderblue, :purple, :rebeccapurple, :rosybrown, :royalblue, :saddlebrown, :salmon, :sandybrown, :seagreen, :seashell, :sienna, :silver, :skyblue, :slateblue, :slategray, :snow, :springgreen, :steelblue, :tan, :teal, :thistle, :tomato, :turquoise, :violet, :webgray, :webgreen, :webmaroon, :webpurple, :wheat, :whitesmoke, :yellowgreen]
+# 
+# 100.times.with_index do |i|
+#   puts "#{i+1} HELLO!".chars.map { |char| Rainbow(char).color(colors.sample) }.join
+# end
+
+
+# https://ruby-doc.org/docs/ruby-doc-bundle/Tutorial/part_02/user_input.html
+# This was fun today with Sophie! Let's
+# do some more lesson-style fun where we
+# follow some simple instructions.
+# -MC 2021-06-19 23:06:03 CDT
+puts ' what is your name'
+name    = gets
+puts 'hello ' + name + '. how are you?'
